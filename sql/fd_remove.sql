ceb337af96984f58b463a4af05028b66971671a1
qwl <qwl@ncmem.com>
2017-05-29 13:45:38 +0800

    删除冗余SQL脚本

---
+++ sql/fd_remove.sql
@@ -0,0 +1,18 @@
+﻿drop procedure if exists fd_remove;
+DELIMITER $$
+/* =============================================
+-- Author:		zysoft
+-- Create date: 2016-08-04
+-- Description:	批量查询相同MD5的文件
+-- =============================================
+*/
+CREATE PROCEDURE fd_remove(
+	 in idSign varchar(36)
+	,in uid int
+)
+BEGIN
+	update up7_files set f_deleted=1 where f_idSign=idSign and f_uid=uid;
+	update up7_files set f_deleted=1 where f_rootSign=idSign and f_uid=uid;
+	update up7_folders set fd_delete=1 where f_idSign=idSign and fd_uid=uid;
+END$$
+DELIMITER;/*--5.7.9版本MySQL必须加这一句，否则包含多条SQL语句的存储过程无法创建成功*/
--- sql/up7_folders.sql
+++ sql/up7_folders.sql
@@ -3,14 +3,14 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2015 年 05 月 26 日 07:53
+-- 生成日期: 2015 年 01 月 30 日 02:36
 -- 服务器版本: 5.0.45
 -- PHP 版本: 5.2.5
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
--- 数据库: `up7`
+-- 数据库: `httpuploader6`
 --
 
 -- --------------------------------------------------------
@@ -19,28 +19,23 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- 表的结构 `up7_folders`
 --
 
-DROP TABLE IF EXISTS `up7_folders`;
-CREATE TABLE IF NOT EXISTS `up7_folders` (
-  `fd_id` 				int(11) NOT NULL auto_increment,
-  `fd_name` 			varchar(50) default '',
-  `fd_pid` 				int(11) default '0',
-  `fd_uid` 				int(11) default '0',
-  `fd_length` 			bigint(19) default '0',
-  `fd_size` 			varchar(50) default '0',
-  `fd_pathLoc` 			varchar(255) default '',
-  `fd_pathSvr` 			varchar(255) default '',
-  `fd_folders` 			int(11) default '0',
-  `fd_files` 			int(11) default '0',
-  `fd_filesComplete` 	int(11) default '0',
-  `fd_complete` 		tinyint(1) default '0',
-  `fd_delete` 			tinyint(1) default '0',
-  `fd_json` 			varchar(20000) default '',
-  `timeUpload` 			timestamp NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  `fd_pidRoot` 			int(11) default '0',
-  `fd_pathRel` 			varchar(255) default '',
-  PRIMARY KEY  (`fd_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- 导出表中的数据 `up7_folders`
---
+CREATE TABLE up7_folders (
+   fd_sign 			char(36) NOT NULL
+  ,fd_name 			varchar(50) default ''
+  ,fd_pidSign 		char(32) default ''
+  ,fd_uid 			int(11) default '0'
+  ,fd_length 		bigint(19) default '0'
+  ,fd_size 			varchar(50) default '0'
+  ,fd_pathLoc 		varchar(255) default ''
+  ,fd_pathSvr 		varchar(255) default ''
+  ,fd_folders 		int(11) default '0'
+  ,fd_files 		int(11) default '0'
+  ,fd_filesComplete int(11) default '0'
+  ,fd_complete 		tinyint(1) default '0'
+  ,fd_delete 		tinyint(1) default '0'
+  ,fd_json 			varchar(20000) default ''
+  ,timeUpload 		timestamp NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
+  ,fd_rootSign		char(36) default ''		 /*--根级uuid*/
+  ,fd_pathRel		varchar(255) default ''  /*--相对路径。基于顶级节点。root\\child\\self*/
+  ,PRIMARY KEY  (fd_sign)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
