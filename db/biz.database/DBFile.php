d19fbfa388bf058bc7e4415735d1810167485e25
qwl <qwl@ncmem.com>
2017-05-31 09:40:35 +0800

    完善文件数据表操作逻辑

--- db/biz.database/DBFile.php
+++ db/biz.database/DBFile.php
@@ -6,6 +6,7 @@
 	更新记录：
 		2012-04-03 创建
 		2014-08-11 更新数据库操作代码。 
+		2017-05-31 精简代码
 */
 class DBFile
 {
@@ -87,115 +88,6 @@ class DBFile
 		$cmd->bindParam(":f_uid",$uid);
 		$db->ExecuteNonQuery($cmd);
 	}
-
-	/// <summary>
-	/// 
-	/// </summary>
-	/// <param name="f_uid"></param>
-	/// <param name="f_id">文件ID</param>
-	function fd_complete($idSvr)
-	{
-		$db = new DbHelper();
-		$cmd =& $db->GetCommand("update up7_files set f_perSvr='100%',f_lenSvr=f_lenLoc,f_complete=1 where f_id=:f_id;");
-		$cmd->bindParam(":f_id",$idSvr);
-		$db->ExecuteNonQuery($cmd);
-	}
-	
-    function fd_fileProcess($uid, $f_id, $f_pos, $lenSvr, $perSvr, $fd_idSvr, $fd_lenSvr,$fd_perSvr,$complete)
-    {
-    	$this->f_process($uid, $f_id, $f_pos, $lenSvr, $perSvr,$complete);
-    	$this->fd_process($uid, $fd_idSvr, $fd_lenSvr,$fd_perSvr);
-    }
-    
-    function fd_process($uid,$fd_idSvr,$fd_lenSvr,$perSvr)
-    {
-        $sql = "call fd_process(:uid,:idSvr,:lenSvr,:per)";
-        $db = $this->db;
-        $cmd =& $db->GetCommand($sql);     
-
-		$cmd->bindParam(":uid", $uid);
-		$cmd->bindParam(":idSvr", $fd_idSvr);
-		$cmd->bindParam(":lenSvr", $fd_lenSvr);
-		$cmd->bindParam(":per", $perSvr);
-
-		$db->ExecuteNonQuery($cmd);
-		return true;
-	}
-
-	/// <summary>
-	/// 更新上传进度
-	/// </summary>
-	///<param name="f_uid">用户ID</param>
-	///<param name="f_id">文件ID</param>
-	///<param name="f_pos">文件位置，大小可能超过2G，所以需要使用long保存</param>
-	///<param name="f_lenSvr">已上传长度，文件大小可能超过2G，所以需要使用long保存</param>
-	///<param name="f_perSvr">已上传百分比</param>
-	function f_process($f_uid,$f_id,$f_pos,$f_lenSvr,$f_perSvr,$complete)
-	{
-		//$sql = "update up7_files set f_pos=?,f_lenSvr=?,f_perSvr=? where f_uid=? and f_id=?";
-		$sql = "call f_process(:pos,:len,:per,:uid,:id,:cmp)";//使用存储过程
-		$db = &$this->db;
-		$cmd =& $db->GetCommand($sql);
-		
-		$cmd->bindParam(":pos",$f_pos);
-		$cmd->bindParam(":len",$f_lenSvr);
-		$cmd->bindParam(":per",$f_perSvr);
-		$cmd->bindParam(":uid",$f_uid);
-		$cmd->bindParam(":id",$f_id);
-		$cmd->bindParam(":cmp",$complete,PDO::PARAM_BOOL);//fix(2016-05-26)
-
-		$db->ExecuteNonQuery($cmd);
-		return true;
-	}
-
-	/// <summary>
-	/// 上传完成。将所有相同MD5文件进度都设为100%
-	/// </summary>
-	function UploadComplete($md5)
-	{
-		$sql = "update up7_files set f_lenSvr=f_lenLoc,f_perSvr='100%',f_complete=1 where f_md5=:f_md5";
-		$db = new DbHelper();
-		$cmd =& $db->GetCommand($sql);
-		
-		$cmd->bindParam(":f_md5", $md5);
-		$db->ExecuteNonQuery($cmd);
-	}
-
-	/// <summary>
-	/// 检查相同MD5文件是否有已经上传完的文件
-	/// </summary>
-	/// <param name="md5"></param>
-	function HasCompleteFile($md5)
-	{
-		//为空
-		if (empty($md5)) return false;
-
-		$sql = "select f_id from up7_files where f_complete=1 and f_md5=:f_md5";
-		$db = new DbHelper();
-		$cmd =& $db->GetCommand($sql);
-
-		$cmd->bindParam(":f_md5", $md5);
-		$ret = $db->ExecuteScalar($cmd);
-
-		return empty($ret);
-	}
-
-	/// <summary>
-	/// 删除一条数据，并不真正删除，只更新删除标识。
-	/// </summary>
-	/// <param name="f_uid"></param>
-	/// <param name="f_id"></param>
-	function Delete($f_uid,$f_id)
-	{
-		$sql = "update up7_files set f_deleted=1 where f_uid=:f_uid and f_id=:f_id";
-		$db = new DbHelper();
-		$cmd =& $db->GetCommand($sql);
-
-		$cmd->bindParam(":f_uid", $f_uid);
-		$cmd->bindParam(":f_id", $f_id);
-		$db->ExecuteNonQuery($cmd);
-	}
-	
 	function remove($idSign)
 	{
 		$sql = "update up7_files set f_deleted=1 where f_idSign=:f_idSign";
@@ -204,91 +96,6 @@ class DBFile
 		
 		$cmd->bindParam(":f_idSign", $idSign);
 		$db->ExecuteNonQuery($cmd);
-	}
-
-	/// <summary>
-	/// 根据根文件夹ID获取未上传完成的文件列表，并转换成JSON格式。
-	/// 说明：
-	///		1.此函数会自动对文件路径进行转码
-	/// </summary>
-	/// <param name="fidRoot"></param>
-	/// <returns></returns>
-	function GetUnCompletesJson($fidRoot)
-	{
-		$sql = "select ";
-		$sql = $sql . "f_nameLoc";
-		$sql = $sql . ",f_pathLoc";
-		$sql = $sql . ",f_lenLoc";
-		$sql = $sql . ",f_sizeLoc";
-		$sql = $sql . ",f_md5";
-		$sql = $sql . ",f_pidRoot";
-		$sql = $sql . ",f_pid";
-		$sql = $sql . " from up7_files where f_pidRoot=:f_pidRoot;";		
-
-		$db = new DbHelper();
-		$cmd =& $db->GetCommand($sql);
-		$cmd->bindParam(":f_pidRoot", $fidRoot);
-		$list = $db->ExecuteDataSet($cmd);
-		
-		$arrFiles = array();
-		foreach($list as $row)
-		{
-			$fi = new FileInf();
-			$fi->nameLoc = $row["f_nameLoc"];
-			$fi->pathLoc = $row["f_pathLoc"];
-			$fi->pathLoc = urlencode($fi->pathLoc);
-			$fi->pathLoc = str_replace("+","%20",$fi->pathLoc);
-			$fi->lenLoc = $row["f_lenLoc"];
-			$fi->sizeLoc = $row["f_sizeLoc"];
-			$fi->md5 = $row["f_md5"];
-			$fi->pidRoot = intval($row["f_pidRoot"]);
-			$fi->pidSvr = intval($row["f_pid"]);
-			array_push($arrFiles,json_encode($fi));
-		}
-		return json_encode($arrFiles);
-	}
-
-	/// <summary>
-	/// 获取未上传完的文件列表
-	/// </summary>
-	/// <param name="fidRoot"></param>
-	/// <param name="files"></param>
-	function GetUnCompletesArr($fidRoot,&$files)
-	{
-		$sql = "select ";
-		$sql = $sql . "f_id";
-		$sql = $sql . ",f_nameLoc";
-		$sql = $sql . ",f_pathLoc";
-		$sql = $sql . ",f_pathSvr";
-		$sql = $sql . ",f_lenLoc";
-		$sql = $sql . ",f_sizeLoc";
-		$sql = $sql . ",f_md5";
-		$sql = $sql . ",f_pidRoot";
-		$sql = $sql . ",f_pid";
-		$sql = $sql . ",f_lenSvr";
-		$sql = $sql . " from up7_files where f_pidRoot=:f_pidRoot and f_complete=0;";
-
-		$db = new DbHelper();	
-		$cmd =& $db->GetCommand($sql);
-		$db->ExecuteNonQueryConTxt("set names utf8");
-		$cmd->bindParam(":f_pidRoot", $fidRoot);
-		
-		$list = $db->ExecuteDataSet($cmd);
-		foreach($list as $row)
-		{
-			$fi				= new FileInf();
-			$fi->idSvr		= intval($row["f_id"]);
-			$fi->nameLoc	= $row["f_nameLoc"];
-			$fi->pathLoc	= $row["f_pathLoc"];
-			$fi->pathSvr	= $row["f_pathSvr"];
-			$fi->lenLoc		= $row["f_lenLoc"];
-			$fi->sizeLoc	= $row["f_sizeLoc"];
-			$fi->md5		= $row["f_md5"];
-			$fi->pidRoot	= intval($row["f_pidRoot"]);
-			$fi->pidSvr		= intval($row["f_pid"]);
-			$fi->lenSvr 	= $row["f_lenSvr"];
-			array_push($files,$fi);
-		}
 	}
 }
 ?>
