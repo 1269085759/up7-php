c9281ee33fd8a45c142dd285c3868baaebba8312
qwl <qwl@ncmem.com>
2017-05-31 14:17:29 +0800

    完善文件夹进度更新逻辑

--- db/fd_update.php
+++ db/fd_update.php
@@ -2,28 +2,24 @@
 ob_start();
 header('Content-Type: text/html;charset=utf-8');
 
-require('../DbHelper.php');
-require('../DBFile.php');
-require('../DBFolder.php');
+require('../biz.redis/RedisTool.php');
+require('../biz.redis/FileRedis.php');
 
-$uid 		= $_GET["uid"];
-$sign 		= $_GET["sign"];
-$fid 		= $_GET["idSvr"];
+$id 		= $_GET["idSign"];
 $perSvr 	= $_GET["perSvr"];
 $lenSvr 	= $_GET["lenSvr"];
-$lenLoc 	= $_GET["lenLoc"];
 
-if(    strlen(uid) < 1 
-	|| strlen(lenSvr) < 1
-	|| strlen(fid) <1
-	|| strlen(perSvr) <1 )
+if(    strlen(id) < 1 
+	|| strlen(perSvr) < 1	
+	|| strlen(lenSvr) <1 )
 {
 	echo "param is null";		
 	die();
 }
 
-$db = new DBFolder();
-$db->update($fid,$perSvr,$lenSvr,$uid);
+$j = RedisTool::con();
+$fr = new FileRedis($j);
+$fr->process($id, $perSvr, $lenSvr, "0", "0");
 
 header('Content-Length: ' . ob_get_length());
 ?>
