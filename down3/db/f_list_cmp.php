456bec059f7055233af99b9972a46e2f6829a228
qwl <qwl@ncmem.com>
2017-06-01 11:40:00 +0800

    完善下载列表加载逻辑

--- down3/db/f_list_cmp.php
+++ down3/db/f_list_cmp.php
@@ -1,18 +1,19 @@
 <?php
 header('Content-Type: text/html;charset=utf-8');
 require('../../biz/PathTool.php');
+require('../../biz.database/DbHelper.php');
+require('../biz/CompleteReader.php');
 
 $uid = $_GET["uid"];
 $cbk = $_GET["callback"];//jsonp
 
 if ( strlen($uid) > 0)
 {
-	$cb = new cmp_builder();
-	$json = $cb->read($uid);	
-	if(!empty($json))
+	$cb = new CompleteReader();
+	$json = $cb->all($uid);		
+	if( !empty($json) )
 	{
-		$json = urlencode($json);
-		$json = str_replace("+", "%20", $json);
+		$json = PathTool::url_encode($json);		
 		echo "$cbk({\"value\":\"$json\"})";
 		return;
 	}
